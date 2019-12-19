7892f576b015a585ff6971a9fd50600413d71971
tiritto <github@niedzwiedzki.tech>
2019-12-20 00:54:25 +0100

    Implementation of few more tasks.

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const util = require('util');
 const path = require('path');
 const fs = require('fs');
 
-const stylus = require('gulp-stylus');
 
 const gettextParser = require('gettext-parser');
 
+
+const src = path.join(__dirname, 'src/');
+const build = path.join(__dirname, 'build/');
+console.log(src);
+return 0;
 
 // Importing gulp and gulpfile as I want to run few of its tasks
 const gulp = require('gulp');
@@ -17,76 +21,27 @@ const dir = "/srv/discord/websites/hotellewd.com/";
 
 
 
-const destination = path.join(__dirname, 'build');
-
-//
-// Compressing JavaScript files 
-//
-const terser = require('gulp-terser');
-gulp.task('js', () => {
-    return gulp.src(dir+'javascript/*.js')
-        .pipe(terser({
-            'booleans_as_integers': true,
-            'drop_console': true, // Discard calls to console.* functions
-            'ecma': 6,
-            'passes': 5,
-            'unsafe_arrows': true, // Convert ES5 style anonymous function expressions to arrow functions if the function body does not reference 'this'
-            'unsafe_math': true, // Optimize numerical expressions like 2 * x * 3 into 6 * x, which may give imprecise floating point results
-            'unsafe_methods': true, // Converts { m: function(){} } to { m(){} }.
-
-        }))
-        .pipe(gulp.dest(dir+'public/'));
-});
-
-// 
-// Converting Stylus files into CSS and compressing resulted CSS files 
-//
-const stylus   = require('gulp-stylus');
-const cleanCSS = require('gulp-clean-css');
-const stylusDirectory = path.join(__dirname, 'src/stylus/');
-const stylusFiles = stylusDirectory+ '*.styl';
-gulp.task('css', () => {
-    console.log('Converting stylus files into CSS and compressing resulted CSS files...');
-    return gulp.src(stylusFiles)
-        .pipe(stylus())
-        .pipe(cleanCSS({
-            level: {
-                1: {
-                    all: true,
-                    roundingPrecision: 2
-                },
-                2: {
-                    all: true,
-                    restructureRules: true
-                }
-            }
-        }))
-        .pipe(gulp.dest(destination));
-});
-
-//
-// Default GULP task
-// 
-// While it might not be the best idea to connect default task of gulp to it's watcher methods,
-// this feels like the easiest and most convinient solution in case of my personal website where
-// I am the only person working on that "Project".
-//
-gulp.task('default', () => {
-    gulp.watch(stylusFiles, gulp.series('css'));
-    gulp.watch(dir+'javascript/*.js', gulp.series('js'));
-});
+const destination = path.join(__dirname, 'build/');
 
 // Promisifying few methods for later use
 const readDirectory = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
 
+// Prepare few functions that Im going to use later on
+function getTranslation(translationObject, desiredString) {
+    return translationObject.translations[''][desiredString].msgstr[0] || "TRANSLATION_NOT_FOUND";
+}
+
+// Rendering PUG files into static HTML pages with multiple translation variants
+gulp.task('html', () => {
+
+
+
 /*
  * I'm declaring some functions that will come in handy later.
 /*/
 
-function getTranslation(translationObject, desiredString) {
-    return translationObject.translations[''][desiredString].msgstr[0] || null;
-}
+
 
 // Check for available translation files
 console.log("Checking available translations in /src/lang directory...");
@@ -134,6 +89,112 @@ readDirectory(translationsPath).then(async files => {
     }
 
 }).catch(console.error);
+
+});
+
+//
+// Compressing images  
+//
+const imagemin = require('gulp-imagemin');
+const imageSource = src + 'img/*';
+gulp.task('img', () => {
+    return gulp.src(imageSource)
+        .pipe(imagemin([
+
+            // GIF Optimizer: https://github.com/imagemin/imagemin-gifsicle
+            imagemin.gifsicle({
+                interlaced: false,
+                optimizationLevel: 3
+            }),
+
+            // JPEG Optimizer: https://github.com/imagemin/imagemin-jpegtran
+            imagemin.jpegtran({
+                progressive: false,
+                arithmetic: true // TODO: Check browser support for this
+            }),
+
+            // PNG Optimizer: https://github.com/imagemin/imagemin-optipng
+            imagemin.optipng({
+                optimizationLevel: 7, // 240 trials for production
+                bitDepthReduction: true,
+                colorTypeReduction: true,
+                paletteReduction: true,
+                interlaced: false
+            }),
+
+            // SVG Optimizer: https://github.com/imagemin/imagemin-svgo
+            imagemin.svgo({
+                removeRasterImages: true, // There should be no raster images within SVG files in this project in the first place...
+                removeScriptElement: true, // Like above, I'm not going to use any scripts within SVG files in this project...
+                removeOffCanvasPaths: true,
+                reusePaths: true,
+                sortAttrs: true // Aside rom readability, it should also help out with compression a little bit
+            })
+        ]))
+        .pipe(gulp.dest(destination+'public/'));
+});
+
+//
+// Compressing JavaScript files 
+//
+const terser = require('gulp-terser');
+const jsSource = src + 'js/*';
+gulp.task('js', () => {
+    return gulp.src(jsSource)
+        .pipe(terser({
+            'booleans_as_integers': true,
+            'drop_console': true, // Discard calls to console.* functions
+            'ecma': 6,
+            'passes': 5,
+            'unsafe_arrows': true, // Convert ES5 style anonymous function expressions to arrow functions if the function body does not reference 'this'
+            'unsafe_math': true, // Optimize numerical expressions like 2 * x * 3 into 6 * x, which may give imprecise floating point results
+            'unsafe_methods': true, // Converts { m: function(){} } to { m(){} }
+        }))
+        .pipe(gulp.dest(destination));
+});
+
+// 
+// Converting Stylus files into CSS and compressing resulted CSS files 
+//
+const stylus = require('gulp-stylus');
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const cssSource = src + 'img/*';
+gulp.task('css', () => {
+    console.log('Converting stylus files into CSS and compressing resulted CSS files...');
+    return gulp.src(cssSource)
+        .pipe(stylus())
+        .pipe(autoprefixer({
+            flexbox: false, // I'm not supporting old versions of IE on my website
+            // Detailed instructions on how to define custom usage data: https://github.com/browserslist/browserslist#custom-usage-data
+            browsers: '> 0.5%, > 0.25% in PL, last 2 versions, Firefox ESR, cover 97.5% in PL, not dead' // I want near perfect coverage for Poland
+        }))
+        .pipe(cleanCSS({
+            level: {
+                1: {
+                    all: true,
+                    roundingPrecision: 2
+                },
+                2: {
+                    all: true,
+                    restructureRules: true
+                }
+            }
+        }))
+        .pipe(gulp.dest(destination));
+});
+
+//
+// Default GULP task
+// 
+// While it might not be the best idea to connect default task of gulp to it's watcher methods,
+// this feels like the easiest and most convinient solution in case of my personal website where
+// I am the only person working on that "Project".
+//
+gulp.task('default', () => {
+    gulp.watch(cssSource, gulp.series('css'));
+    gulp.watch(dir+'javascript/*.js', gulp.series('js'));
+});
 
 
 // TODO: Convert /src/pug/*.pug files to HTML and move it to /public/*.html
