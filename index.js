b32362ce82c809fbc20a9b0664ec6e89fc0fcbcd
tiritto <github@niedzwiedzki.tech>
2019-12-22 06:32:29 +0100

    Updated HTTP server

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-console.log("Starting website builder...");
-const express = require('express.js');
+const express = require('express');
+const path = require('path');
+const webserver = express();
 
-const webhook = express();
 const childProcess = require('child_process');
 
 const githubUsername = 'tiritto';
 
-webhook.post("/github", (req, res) => {
+webserver.post("/github", (req, res) => {
+    console.log('PRODUCTION PUSH!');
 
     // Right away send back status code 200. There is no point in prologing session.
     res.status(200).send();
@@ -15,7 +16,18 @@ webhook.post("/github", (req, res) => {
 
     if (isProductionBranch && req.body.sender == githubUsername) childProcess.exec(`cd ${__dirname} && ./deploy.sh`, (error, stdout, stderr) => {
 
+        console.log('UPDATE DONE!!');
         // Print error in the console if something goes wrong
         if (error) return console.error(error);
     });
+});
+
+webserver.use(express.static(path.join(__dirname, 'public')));
+
+webserver.get('/', (req, res) => {
+    res.send("Hello world!!! Co tam?");
+});
+
+webserver.listen(3100, () => {
+    console.log('Listening on port 3100!');
 });
